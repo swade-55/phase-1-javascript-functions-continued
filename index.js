23f57533adb510dfb8c238738f550e9a6ebbb81b
SamuelWade <samuelrcwade@gmail.com>
2023-02-24 07:34:06 -0500

    Test Successful

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(message='roller-skate') {
+    return `This Saturday, I want to ${message}!`;
+}
+
+saturdayFun();
+saturdayFun('bathe my dog');
+
+function mondayWork(message=`go to the office`) {
+    return `This Monday, I will ${message}.`;
+}
+
+mondayWork();
+mondayWork(`work from home.`);
+
+function wrapAdjective(special='*') {
+    const part1 = 'You are';
+    return function (phrase) {
+        return (`${part1} ${special}${phrase}${special}!`);
+        }
+    }
+
+wrapAdjective()('a hard worker');
+wrapAdjective('||')('a dedicated programmer');
